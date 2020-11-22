ce7f4d1a67ddbcc8ef642351861bb1bcca35d966
SemaDevpy <syimykdev@gmail.com>
2020-11-22 21:39:29 +0600

    Category screen basic design, create and read operation with core data.

---
+++ TakeNote/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  TakeNote
+//
+//  Created by Syimyk on 11/22/20.
+//  Copyright © 2020 Syimyk. All rights reserved.
+//
+
+import Foundation
+
+
+struct K {
+    static let firstCell = "categoryCell"
+    static let segue = "goToNotes"
+}
---
+++ TakeNote/Controller/CategoryViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  TakeNote
+//
+//  Created by Syimyk on 11/22/20.
+//  Copyright © 2020 Syimyk. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+
+class CategoryViewController: UITableViewController {
+    
+    var categories = [Category]()
+    var context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadCategories()
+    }
+    
+    //MARK: - Adding new category(Create in CRUD)
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add Category", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add a new category", style: .default) { (alertAtion) in
+            let newCategory = Category(context: self.context)
+            newCategory.name = textField.text!
+            self.categories.append(newCategory)
+            self.saveCategory()
+        }
+        
+        alert.addTextField { (innerTextField) in
+            textField = innerTextField
+            textField.placeholder = "Enter a new category"
+        }
+        alert.addAction(action)
+        present(alert, animated: true)
+    }
+    
+    
+    //MARK: - TableView Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.firstCell, for: indexPath)
+        cell.textLabel?.text = categories[indexPath.row].name
+        return cell
+    }
+    
+    
+    //MARK: - Data Manupulation Methods
+    func saveCategory(){
+        do {
+            try context.save()
+        }catch{
+            print("Error saving context \(error)")
+        }
+        tableView.reloadData()
+    }
+    
+    //Read in CRUD
+    func loadCategories(){
+        let request : NSFetchRequest<Category> = Category.fetchRequest()
+        do{
+            categories = try context.fetch(request)
+        }catch{
+            print("Error in fetching data\(error)")
+        }
+        tableView.reloadData()
+    }
+    
+}
+
